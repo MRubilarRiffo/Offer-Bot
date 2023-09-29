5894d29ad2a7568d27f1f5173e4ddbcb04baf4ca
Mauricio A. Rubilar <m.rubilar1010@gmail.com>
2023-09-29 00:24:42 -0300

    add multiprocess BOT

--- Bot/main.py
+++ Bot/main.py
@@ -1,19 +1,23 @@
-import asyncio
+from multiprocessing import Process
 from config.settings import sortOrder, fields, filters_premium, filters_free, CANAL_PREMIUM_ID, CANAL_FREE_ID, TOKEN_FREE, TOKEN_PREMIUM
 from handlers.send_data import send_data
 from utilities.log_message import log_message
+import time
 
 SLEEP = 5 # minutes
 
-async def check_api_every_minute():
+def send_data_every_minute(token, sortOrder, fields, canal_id, filters):
     while True:
-        task_premium = asyncio.create_task(send_data(TOKEN_PREMIUM, sortOrder, fields, CANAL_PREMIUM_ID, filters_premium))
-        task_free = asyncio.create_task(send_data(TOKEN_FREE, sortOrder, fields, CANAL_FREE_ID, filters_free))
-
-        await asyncio.gather(task_premium, task_free)
-
+        send_data(token, sortOrder, fields, canal_id, filters)
         log_message('Esperando...')
-        await asyncio.sleep(SLEEP * 60)
+        time.sleep(SLEEP * 60)
 
 if __name__ == '__main__':
-    asyncio.run(check_api_every_minute())
+    process_premium = Process(target=send_data_every_minute, args=(TOKEN_PREMIUM, sortOrder, fields, CANAL_PREMIUM_ID, filters_premium))
+    process_free = Process(target=send_data_every_minute, args=(TOKEN_FREE, sortOrder, fields, CANAL_FREE_ID, filters_free))
+
+    process_premium.start()
+    process_free.start()
+
+    process_premium.join()
+    process_free.join()
